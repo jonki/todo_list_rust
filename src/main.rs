38a57f8d33ada465ff6d4a27f495db9c54a6b651
jonki <andrzej.fricze@gmail.com>
2021-01-18 16:55:19 +0100

    Use env arguments to choose file storing todos. Remove casting enum to i8

--- src/main.rs
+++ src/main.rs
@@ -6,25 +6,34 @@
 
 extern crate ncurses;
 
-use std::fs;
-use std::io::prelude::*;
 use ncurses::*;
-use std::path::Path;
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 use serde_json;
+use std::env;
+use std::fs;
 use std::fs::OpenOptions;
-
-const FILENAME: &str = ".todos.json";
+use std::io::prelude::*;
+use std::path::Path;
 
 fn main() {
-    let current_working_dir: &str = &std::env::current_dir().unwrap().into_os_string().into_string().unwrap();
+    let filename = if let Some(arg1) = env::args().nth(1) {
+        arg1
+    } else {
+        String::from(".todo.json")
+    };
+
+    let current_working_dir: &str = &std::env::current_dir()
+        .unwrap()
+        .into_os_string()
+        .into_string()
+        .unwrap();
     let mut todos: Vec<Todo> = Vec::new();
     let mut cur_index: i32 = 0;
-    let mut screen: i8 = SCREEN::MAIN as i8; // Set the screen
+    let mut screen: SCREEN = SCREEN::MAIN; // Set the screen
 
     // Check if the todos.json file already exists
-    if Path::new(&(current_working_dir.to_owned() + "/".into() + FILENAME.into())).exists() {
-        todos = deserialize_todos();
+    if Path::new(&(current_working_dir.to_owned() + "/".into() + &filename)).exists() {
+        todos = deserialize_todos(&filename);
     }
 
     let bw: WINDOW = initscr();
@@ -38,29 +47,42 @@ fn main() {
         addstr("-------------------------------------------------------------------\n");
         addstr("a: Add, e: Edit, d: Delete, x: Done/Undone, j: DOWN, k: UP, q: Quit\n\n");
 
-        if todos.is_empty() && screen == SCREEN::MAIN as i8 {
+        if todos.is_empty() && screen == SCREEN::MAIN {
             addstr("--- **NOTHING TODO** ---\n");
         }
 
-        if screen == SCREEN::MAIN as i8 {
-            list_todos(&todos, cur_index);
-            // Listens for key
-            listen_key(&mut cur_index, todos.len() as i32, &mut screen, &mut todos);
-        } else if screen == SCREEN::ADD as i8 {
-            show_add_input(&mut todos, &mut screen, bw, -1);
-        } else if screen == SCREEN::EDIT as i8 {
-            show_add_input(&mut todos, &mut screen, bw, cur_index);
+        match screen {
+            SCREEN::MAIN => {
+                list_todos(&todos, cur_index);
+                // Listens for key
+                listen_key(
+                    &mut cur_index,
+                    todos.len() as i32,
+                    &mut screen,
+                    &mut todos,
+                    &filename,
+                );
+            }
+            SCREEN::ADD => {
+                show_add_input(&mut todos, &mut screen, bw, -1, &filename);
+            }
+            SCREEN::EDIT => {
+                show_add_input(&mut todos, &mut screen, bw, cur_index, &filename);
+            }
         }
+
         refresh();
         clear();
     }
+
     endwin();
 }
 
+#[derive(PartialEq)]
 enum SCREEN {
     MAIN,
     ADD,
-    EDIT
+    EDIT,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -78,15 +100,22 @@ impl Todo {
     }
 }
 
-fn add_todo(todo: &str, todos: &mut Vec<Todo>) {
+fn add_todo(todo: &str, todos: &mut Vec<Todo>, filename: &str) {
     todos.push(Todo {
         todo: todo.to_string(),
         done: false,
     });
-    write_todo(&todos);
+
+    write_todo(&todos, filename);
 }
 
-fn listen_key(mut cur_index: &mut i32, max: i32, screen: &mut i8, mut todos: &mut Vec<Todo>) {
+fn listen_key(
+    mut cur_index: &mut i32,
+    max: i32,
+    screen: &mut SCREEN,
+    mut todos: &mut Vec<Todo>,
+    filename: &str,
+) {
     enum KEY {
         J = 106,
         K = 107,
@@ -95,7 +124,7 @@ fn listen_key(mut cur_index: &mut i32, max: i32, screen: &mut i8, mut todos: &mu
         A = 97,
         D = 100,
         E = 101,
-        ENTER = 10
+        ENTER = 10,
     }
 
     noecho();
@@ -119,21 +148,31 @@ fn listen_key(mut cur_index: &mut i32, max: i32, screen: &mut i8, mut todos: &mu
         *cur_index = -1;
     } else if k == KEY::A as i32 {
         // Add
-        *screen = SCREEN::ADD as i8;
+        *screen = SCREEN::ADD;
     } else if k == KEY::X as i32 {
         // Do/Undo
-        do_undo(*cur_index, &mut todos);
+        do_undo(*cur_index, &mut todos, filename);
     } else if k == KEY::D as i32 {
-        delete_todo(&mut cur_index, &mut todos);
+        delete_todo(&mut cur_index, &mut todos, filename);
     } else if k == KEY::E as i32 || k == KEY::ENTER as i32 {
         if !todos.is_empty() {
-            *screen = SCREEN::EDIT as i8;
+            *screen = SCREEN::EDIT;
         }
     }
 }
 
-fn show_add_input(mut todos: &mut Vec<Todo>, screen: &mut i8, window: WINDOW, mut cur_index: i32) {
-    let mut todo: String = if cur_index >= 0 { (*todos[cur_index as usize].todo).into() } else { String::new() };
+fn show_add_input(
+    mut todos: &mut Vec<Todo>,
+    screen: &mut SCREEN,
+    window: WINDOW,
+    mut cur_index: i32,
+    filename: &str,
+) {
+    let mut todo: String = if cur_index >= 0 {
+        (*todos[cur_index as usize].todo).into()
+    } else {
+        String::new()
+    };
     // let mut todo: String = String::new();
     addstr("Enter Todo: ");
 
@@ -161,14 +200,14 @@ fn show_add_input(mut todos: &mut Vec<Todo>, screen: &mut i8, window: WINDOW, mu
     }
 
     if !todo.is_empty() && cur_index == -1 {
-        add_todo(&todo, &mut todos);
+        add_todo(&todo, &mut todos, filename);
     } else if !todo.is_empty() && cur_index >= 0 {
-        update_todo(&todo, &mut todos, cur_index);
+        update_todo(&todo, &mut todos, cur_index, filename);
     } else if todo.is_empty() && cur_index >= 0 {
-        delete_todo(&mut cur_index, &mut todos);
+        delete_todo(&mut cur_index, &mut todos, filename);
     }
 
-    *screen = SCREEN::MAIN as i8;
+    *screen = SCREEN::MAIN;
 
     curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBLE); // Don't show the terminal cursor
 }
@@ -180,12 +219,13 @@ fn list_todos(todos: &[Todo], cur_index: i32) {
     }
 }
 
-fn do_undo(cur_index: i32, todos: &mut Vec<Todo>) {
+fn do_undo(cur_index: i32, todos: &mut Vec<Todo>, filename: &str) {
     todos[cur_index as usize].done = !todos[cur_index as usize].done;
-    write_todo(&todos);
+
+    write_todo(&todos, filename);
 }
 
-fn delete_todo(cur_index: &mut i32, todos: &mut Vec<Todo>) {
+fn delete_todo(cur_index: &mut i32, todos: &mut Vec<Todo>, filename: &str) {
     let len = todos.len() as i32;
     todos.remove(*cur_index as usize);
     if *cur_index == len - 1 {
@@ -195,37 +235,46 @@ fn delete_todo(cur_index: &mut i32, todos: &mut Vec<Todo>) {
             *cur_index -= 1;
         }
     }
-    write_todo(&todos);
+
+    write_todo(&todos, filename);
 }
 
-fn update_todo(todo: &str, todos: &mut Vec<Todo>, cur_index: i32) {
+fn update_todo(todo: &str, todos: &mut Vec<Todo>, cur_index: i32, filename: &str) {
     todos[cur_index as usize].todo = todo.into();
-    write_todo(&todos);
+
+    write_todo(&todos, filename);
 }
 
-fn open_json() -> String {
-    let current_working_dir: &str = &std::env::current_dir().unwrap().into_os_string().into_string().unwrap();
-    let full_path: String = current_working_dir.to_owned() + "/".into() + FILENAME.into();
+fn open_json(filename: &str) -> String {
+    let current_working_dir: &str = &std::env::current_dir()
+        .unwrap()
+        .into_os_string()
+        .into_string()
+        .unwrap();
+    let full_path: String = current_working_dir.to_owned() + "/".into() + filename.into();
     let path = Path::new(&full_path);
 
     fs::read_to_string(path).unwrap()
 }
 
-fn write_todo(todos: &Vec<Todo>) {
-    let current_working_dir: &str = &std::env::current_dir().unwrap().into_os_string().into_string().unwrap();
-    let full_path: String = current_working_dir.to_owned() + "/".into() + FILENAME.into();
+fn write_todo(todos: &Vec<Todo>, filename: &str) {
+    let current_working_dir: &str = &std::env::current_dir()
+        .unwrap()
+        .into_os_string()
+        .into_string()
+        .unwrap();
+    let full_path: String = current_working_dir.to_owned() + "/".into() + filename.into();
     let path = Path::new(&full_path);
 
     match fs::write(path, serialize_todos(&todos)) {
         Err(e) => {
             panic!("Cannot write to file: {:?}", e)
-        },
-        Ok(_) => {
         }
+        Ok(_) => {}
     };
 
     if path.exists() {
-        add_to_gitignore();
+        add_to_gitignore(filename);
     }
 }
 
@@ -233,25 +282,28 @@ fn serialize_todos(todos: &Vec<Todo>) -> String {
     serde_json::to_string_pretty(&todos).unwrap()
 }
 
-fn deserialize_todos() -> Vec<Todo> {
-    serde_json::from_str(&open_json()).unwrap()
+fn deserialize_todos(filename: &str) -> Vec<Todo> {
+    serde_json::from_str(&open_json(filename)).unwrap()
 }
 
-fn add_to_gitignore() {
-    let current_working_dir: &str = &std::env::current_dir().unwrap().into_os_string().into_string().unwrap();
+fn add_to_gitignore(filename: &str) {
+    let current_working_dir: &str = &std::env::current_dir()
+        .unwrap()
+        .into_os_string()
+        .into_string()
+        .unwrap();
     let full_path: String = current_working_dir.to_owned() + "/".into() + ".gitignore".into();
     let path = Path::new(&full_path);
 
     if path.exists() {
-        if !fs::read_to_string(path).unwrap().contains(FILENAME) {
+        if !fs::read_to_string(path).unwrap().contains(filename) {
             let mut file = OpenOptions::new()
                 .write(true)
                 .append(true)
                 .open(".gitignore")
                 .unwrap();
 
-
-            if let Err(e) = writeln!(file, "{}", FILENAME) {
+            if let Err(e) = writeln!(file, "{}", filename) {
                 eprintln!("Couldn't write to file: {}", e);
             }
         }
